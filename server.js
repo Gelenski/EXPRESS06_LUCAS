a4ec5dc6684976b616e4e10263e5bcec84239301
Lucas Gelenski <gelenskilucas@gmail.com>
2024-08-26 09:35:25 -0300

    final aula 06

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mysql = require("mysql2");
 const mysql_config = require("./mysql_config");
 const functions = require("./functions");
@@ -10,13 +11,15 @@ app.listen(3000, () => {
 
 const connection = mysql.createConnection(mysql_config);
 
+app.use(cors());
+
 app.get("/", (req, res) => {
   connection.query("SELECT * FROM tasks", (err, results) => {
     if (err) {
       res.json(functions.response("error", "Erro: " + err.message));
     } else {
       res.json(
-        functions.response("sucess", "tasks listadas com sucesso", results)
+        functions.response("success", "tasks listadas com sucesso.", results)
       );
     }
   });
